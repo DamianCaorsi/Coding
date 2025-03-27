83f0b2862e41b6127dc62dd4a3167994be6f211f
Damian De los santos Caorsi <caordamianjob@gmail.com>
2025-03-27 06:29:02 -0300

    dia 3

---
+++ Page/JavaScript/Code.js
@@ -0,0 +1,9 @@
+// Basic JavaScript Code Example
+
+// Function to greet a user
+function greetUser(name) {
+    console.log(`Hello, ${name}! Welcome to JavaScript.`);
+}
+
+// Call the function
+greetUser("User");
